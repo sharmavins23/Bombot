99a24be5d7bf747fdd71e80a81165b02c132f2a7
sharmavins23 <sharmavins23@gmail.com>
2024-12-06 16:38:02 -0800

    rewriting old functionality

--- src/main.ts
+++ src/main.ts
@@ -1,74 +1,48 @@
-import { dirname, importx } from "@discordx/importer";
 import chalk from "chalk";
-import type { Message } from "discord.js";
-import { IntentsBitField } from "discord.js";
-import { Client } from "discordx";
+import { Client, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
-import { getGitCommitHash } from "./utils/git.js";
+import { getGitCommitHash } from "./utils/Git.js";
+import { LogX } from "./utils/Logging.js";
 
 // ===== Configuration =========================================================
 
 // Check if running in development environment or not
-const isDev = process.env.RUNTIME_ENV == "beta";
+export const isDev = process.env.RUNTIME_ENV == "beta";
+export const botName = isDev ? "Testbot" : "Bombot";
 
 // Create a new bot
-export const bot = new Client({
+const client = new Client({
     intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildMessageReactions,
-        IntentsBitField.Flags.GuildVoiceStates,
-        IntentsBitField.Flags.MessageContent,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent,
     ],
-    silent: false, // Enable debug logs always
-    simpleCommand: {
-        prefix: [
-            // Tag @bot
-            `<@${process.env.BOT_CLIENT_ID}>`,
-            // Different commands for beta or prod
-            isDev ? "testbot," : "bombot,",
-            // Simplified non-comma for lazy users
-            isDev ? "testbot" : "bombot",
-            // Uppercase variants for mobile users
-            isDev ? "Testbot," : "Bombot,",
-            isDev ? "Testbot" : "Bombot",
-        ],
-    },
 });
 
 // ===== OnEvent handlers ======================================================
 
 // Ready handler
-bot.once("ready", () => {
-    // Synchronize any application commands with Discord API
-    bot.initApplicationCommands();
-
-    // Print out latest version
-    console.log(chalk.green("Bombot started!"));
-    console.log(`Latest deployment: ${chalk.cyan(getGitCommitHash())}`);
-});
-
-// Message handler
-bot.on("messageCreate", (message: Message) => {
-    // Handle any simple commands
-    bot.executeCommand(message);
-});
+client.once("ready", () => {});
 
 // ===== Executables ===========================================================
 
 // Run!
 async function run() {
-    // Import all events and commands
-    await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
+    LogX.assert(
+        typeof process.env.BOT_TOKEN !== "undefined",
+        "Could not find bot token in your environment!",
+    );
 
-    // Handle the bot separately depending on dev vs prod
-    // Start the bot
-    if (!process.env.BOT_TOKEN) {
-        throw Error("Could not find bot token in your environment!");
-    }
+    const loginOutput: string = await client.login(process.env.BOT_TOKEN);
+    LogX.assert(
+        loginOutput === process.env.BOT_TOKEN,
+        "Failed to login to Discord!",
+    );
 
-    await bot.login(process.env.BOT_TOKEN);
+    LogX.log(`${botName} is now succesfully`, chalk.green("logged in") + "!");
+    LogX.logD(`Latest deployment: ${chalk.cyan(getGitCommitHash())}`);
 }
 
 run();
